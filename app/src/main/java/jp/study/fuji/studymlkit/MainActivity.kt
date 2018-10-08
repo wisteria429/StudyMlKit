24bd0e2d395fc1c7c3591b20171d3931e9fd27a5
fuji <wisteriaroots429@gmail.com>
2018-10-09 01:06:23 +0900

    init commit

---
+++ app/src/main/java/jp/study/fuji/studymlkit/MainActivity.kt
@@ -0,0 +1,23 @@
+package jp.study.fuji.studymlkit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val options = FirebaseVisionFaceDetectorOptions.Builder()
+                .setModeType(FirebaseVisionFaceDetectorOptions.ACCURATE_MODE)
+                .setLandmarkType(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                .setMinFaceSize(0.15f)
+                .setTrackingEnabled(true)
+                .build()
+    }
+}
